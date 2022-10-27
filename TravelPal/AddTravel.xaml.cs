c1ad843b1d51ea92cee6541664a0f1ac87a8a693
Minderg <105292841+Minderg@users.noreply.github.com>
2022-10-27 18:46:14 +0200

    Update

--- TravelPal/AddTravel.xaml.cs
+++ TravelPal/AddTravel.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TravelPal.Classes;
+using TravelPal.Enums;
+using TravelPal.Travels;
 
 namespace TravelPal
 {
@@ -19,9 +23,27 @@ namespace TravelPal
     /// </summary>
     public partial class AddTravel : Window
     {
+        private UserManager userManager;
         public AddTravel()
         {
             InitializeComponent();
+
+            // Lägger till Länder i comboboxen
+            string[] countries = Enum.GetNames(typeof(Countries));
+
+            cbAddCountry.ItemsSource = countries;
+
+            this.userManager = userManager;
+
+            // Lägger till Work/Leisure i combobox
+
+            string[] tripTypes = Enum.GetNames(typeof(TripTypes));
+
+            cbChoose.ItemsSource = tripTypes;
+
+            this.userManager = userManager;
+
         }
+
     }
 }
